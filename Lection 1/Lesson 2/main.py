354e86d273d806b178e5e40575d1a5f003137956
twix2sweet <twix2sweet@gmail.com>
2023-10-03 22:14:18 +0300

    Lesson 1 Task 2 - Added

---
+++ Lection 1/Lesson 2/main.py
@@ -0,0 +1,7 @@
+print( "This program will add entered numbers. To stop adding number and see result please type 'stop' :)\n" )
+
+firstNumber = float( input( "Please enter first number to add: " ) )
+secondNumber = float( input( "Please enter second number to add: " ) )
+result = firstNumber + secondNumber
+
+print( "Your result: {}".format( result ) )
