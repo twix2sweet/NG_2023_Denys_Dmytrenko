9927b2b65127d811a19bf64cdd2dd05aa35e9b8e
twix2sweet <twix2sweet@gmail.com>
2023-10-07 16:58:57 +0300

    Lesson 1 Task 4 - Add forgotten operations

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,9 +1,9 @@
 print( "This program is a simple calculator." )
 
-operationTypeInput = input( "What arithmetic operation do you want to perform?\n`*` - Multiplication\n`/` - Division\n`+` - Addition\n`-` - Subtraction\n" )
+operationTypeInput = input( "What arithmetic operation do you want to perform?\n`*` - Multiplication\n`/` - Division\n`+` - Addition\n`-` - Subtraction\n`root` - Root\n`pow` - Raise to power" )
 
 firstNumber = float( input( "Please enter first number: " ) )
-secondNumber = float( input( "Please enter second number: " ) )
+secondNumber = float( input( "Please enter second number : " ) )
 
 match operationTypeInput:
     case "*":
@@ -14,6 +14,10 @@ match operationTypeInput:
            result = firstNumber + secondNumber
     case "-":
            result = firstNumber - secondNumber
+    case "root":
+           result = firstNumber ** (1 / secondNumber)
+    case "pow":
+           result = pow( firstNumber, secondNumber )
     case _:
            print("Oh, dear...\n{} - is not an ariphmetic operation!".format( operationTypeInput ))
            exit()
